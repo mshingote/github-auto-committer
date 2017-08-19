45d92f9ea1c92c2edf9c8b12378f85b3cdc1c057
Alex Bod <alex@alexbod.com>
2017-08-19 13:22:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ def main(dirs, dates):
 
 
 if __name__ == "__main__":
-    main({'github_auto_commiter':'https://github.com/gabontau/github_auto_commiter.git'},
+    main({'github_auto_commiter':'https://github.com/alexbod/github-auto-commiter.git'},
          (date(2017,2,23), date(2017,2,25)))
